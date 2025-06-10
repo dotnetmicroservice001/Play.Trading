45996e5bace3b20e8aa15fa6c4710323aca2a2b6
basnets24 <sneha.basnet2@gmail.com>
2025-06-10 13:49:34 -0400

    configuring trading microservice

--- Play.Trading.Service/Startup.cs
+++ Play.Trading.Service/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.Identity;
+using Play.Common.MassTransit;
+using Play.Common.MongoDB;
+using Play.Common.Settings;
+using Play.Trading.Service.StateMachines;
 
 namespace Play.Trading.Service
 {
@@ -27,6 +33,11 @@ namespace Play.Trading.Service
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddMongo()
+                .AddJwtBearer();
+           AddMassTransit(services);
+           
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -47,12 +58,32 @@ namespace Play.Trading.Service
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddMassTransit(IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                // saga specific configuration
+                configure.UsingPlayEconomyRabbitMq();
+                configure.AddSagaStateMachine<PurchaseStateMachine, PurchaseState>()
+                    .MongoDbRepository(r =>
+                        {
+                            var serviceSettings = Configuration.GetSection(nameof(ServiceSettings))
+                                .Get<ServiceSettings>();
+                            var mongoSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                           r.Connection = mongoSettings.ConnectionString;
+                           r.DatabaseName = serviceSettings.ServiceName;
+                        });
+                // to open up the bus through which messages are going to go 
+                services.AddMassTransitHostedService();
             });
         }
     }
