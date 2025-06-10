810a0523000d07c4b8ffc1b39b992e06743108e2
basnets24 <sneha.basnet2@gmail.com>
2025-06-10 13:02:14 -0400

    initializing the state machine

--- Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
+++ Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -1,3 +1,4 @@
+using System;
 using Automatonymous;
 
 namespace Play.Trading.Service.StateMachines;
@@ -12,8 +13,35 @@ public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
 
     public State Faulted { get; set; }
 
+    // declare an event
+    public Event<PurchaseRequested> PurchaseRequested { get;  }
     public PurchaseStateMachine()
     {
         InstanceState(state => state.CurrentState);
+        ConfigureEvents();
+    }
+
+    private void ConfigureEvents()
+    {
+        Event(() => PurchaseRequested); 
+    }
+
+    private void ConfigureInitialState()
+    {
+        Initially(
+            When(PurchaseRequested)
+                .Then(context =>
+                {
+                    // data = Incoming event/message payload
+                    // instance = current state machine instance
+                    // (a persisted object often called a saga)
+                    context.Instance.UserId = context.Data.UserId;
+                    context.Instance.ItemId = context.Data.ItemId;
+                    context.Instance.Quantity = context.Data.Quantity;
+                    context.Instance.Received = DateTimeOffset.UtcNow;
+                    context.Instance.LastUpdated = context.Instance.Received;
+                })
+                .TransitionTo(Accepted)
+            );
     }
 }
