3e1206fd40802ca8656c41855fac95f1ca1d72f2
basnets24 <sneha.basnet2@gmail.com>
2025-06-10 12:49:55 -0400

    Mass Transit State Machine

---
+++ Play.Trading.Service/StateMachines/PurchaseState.cs
@@ -0,0 +1,29 @@
+using System;
+using Automatonymous;
+using MassTransit.Saga;
+
+namespace Play.Trading.Service.StateMachines;
+
+public class PurchaseState : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+
+    public string CurrentState { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public Guid ItemId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTimeOffset Received { get; set; }
+    
+    public decimal? PurchaseTotal { get; set; }
+    
+    // to keep track of when it was last updated
+    public DateTimeOffset? LastUpdated { get; set; }
+    
+    public string ErrorMessage { get; set; }
+    
+    public int Version { get; set; }
+}
---
+++ Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -0,0 +1,19 @@
+using Automatonymous;
+
+namespace Play.Trading.Service.StateMachines;
+
+public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
+{
+    public State Accepted { get; }
+    
+    public State ItemsGranted { get; }
+    
+    public State Completed { get; }
+
+    public State Faulted { get; set; }
+
+    public PurchaseStateMachine()
+    {
+        InstanceState(state => state.CurrentState);
+    }
+}
