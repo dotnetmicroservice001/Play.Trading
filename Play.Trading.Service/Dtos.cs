4a6934c44bcfd6eafc415897815fe9a0b8c9d625
basnets24 <sneha.basnet2@gmail.com>
2025-06-10 13:06:44 -0400

    dtos

---
+++ Play.Trading.Service/Dtos.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Play.Trading.Service;
+
+public record SubmitPurchaseDto( 
+    [Required]Guid? ItemId, 
+    [Range(1,100)]int Quantity); 
+
+
+
